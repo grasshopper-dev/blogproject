9603acf3705b7998cb9a283a9c8f7ccfc7105b94
grasshopper-dev <grasshopperdevel2019@gmail.com>
2021-03-12 10:42:04 +0900

    レイアウトを修正、初期表示画面を設定

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -4,6 +4,7 @@ from .views import BlogList, BlogDetail, BlogCreate, BlogDelete, BlogUpdate
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('',  BlogList.as_view(), name='list'),
     path('list/',  BlogList.as_view(), name='list'),
     path('detail/<int:pk>/',  BlogDetail.as_view(), name='detail'),
     path('create/',  BlogCreate.as_view(), name='create'),
